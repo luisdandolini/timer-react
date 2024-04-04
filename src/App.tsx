4715f5a354d6972aacd00f9f3632182828782497
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-04-03 21:34:04 -0300

    feat: create default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+
 
 export function App() {
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="danger" />
-        <Button variant="success" />
-        <Button />
-        
-        <GlobalStyle />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={defaultTheme}>
+          <Router />
+          <GlobalStyle />
+        </ThemeProvider>
+      </BrowserRouter>
     </>
   )
 }
