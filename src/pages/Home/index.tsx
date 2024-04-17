a16afe9c971427000c81d7ebdc0167a4c8b9585a
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-04-16 22:39:45 -0300

    fix: bug seconds with start new project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,13 +47,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         );
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
@@ -66,6 +72,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(newCycle.id);
+    setAmountSecondsPassed(0);
 
     reset();
   }
